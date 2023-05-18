735b97a92e495e0c1fe17a818bb06c3318badab3
liugddx <liugddx@gmail.com>
2023-05-18 18:53:02 +0800

    test

--- tools/ci/sonar_check.sh
+++ tools/ci/sonar_check.sh
@@ -14,13 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [ ! "$SONAR_TOKEN" ]; then
-  echo "SONAR_TOKEN environment is null, skip check"
-  exit 0
-fi
 ./mvnw --batch-mode verify sonar:sonar \
 -Dsonar.host.url=https://sonarcloud.io \
--Dsonar.organization=Guangdong Liu \
+-Dsonar.organization=liugddx \
 -Dsonar.login=63e341bf9ba2e9721f29dca7266cec9f7e605377 \
 -Dsonar.projectKey=liugddx_incubator-paimon \
 -Dsonar.core.codeCoveragePlugin=jacoco \
